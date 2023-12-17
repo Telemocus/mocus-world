d9e8b18dce309880c2c9a186995f18d9a97b7170
Mocus <mocussmncube@gmail.com>
2023-12-17 12:17:19 +0200

    I was debugging an issue, found the issue -> use square brackets when passing an array. new Graph([ ]);

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 myCanvas.width = 600;
 myCanvas.height = 600;
-
 const ctx = myCanvas.getContext("2d");
 
-const graph = new Graph();
+const p1 = new Point(200, 200);
+const p2 = new Point(500, 500);
+const p3 = new Point(350, 350);
+const p4 = new Point(100, 300);
+
+
+const graph = new Graph([p1, p2, p3, p4]);
 graph.draw(ctx);
+
--- js/math/graph.js
+++ js/math/graph.js
@@ -1,20 +1,17 @@
 // G = ( V , E)
 
 class Graph {
-     constructor(points = [], segments = []){
-          this.points = points;
-          this.segments = segments;
-     }
-     draw(ctx){
-          for ( const seg of this.segments) {
-               seg.draw(ctx);
-               
-          }
+  constructor(points = [ ], segments = [ ]) {
+    this.points = points;
+    this.segments = segments;
+  }
+  draw(ctx) {
+    for (const seg of this.segments) {
+      seg.draw(ctx);
+    }
 
-          for ( const point of this.points) {
-               seg.draw(ctx);
-               
-          }
-     }
-
+    for (const point of this.points) {
+      point.draw(ctx);
+    }
+  }
 }
