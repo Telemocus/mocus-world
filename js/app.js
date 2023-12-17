637c472884f9e3c19298ee72e5d76d5225fda765
Mocus <mocussmncube@gmail.com>
2023-12-17 12:38:53 +0200

    fixed erroe in segment.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,12 @@ const p2 = new Point(500, 500);
 const p3 = new Point(350, 350);
 const p4 = new Point(100, 300);
 
+const s1 = new Segment(p1, p2 );
+const s2 = new Segment(p1, p3);
+const s3 = new Segment(p1, p4 );
+const s4 = new Segment(p2, p3 );
 
-const graph = new Graph([p1, p2, p3, p4]);
+const graph = new Graph([p1, p2, p3, p4], [s1, s2, s3, s4]);
+
 graph.draw(ctx);
 
